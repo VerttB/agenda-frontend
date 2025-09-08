89a299eb14bb3e7a66e77f2261337788c42d7db4
VerttB <alyssonoliveira456@gmail.com>
2025-09-07 21:00:46 -0300

    refactor: Utilizando componente de input e button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { useNavigate } from '@tanstack/react-router'
 import { useState } from 'react'
 import { Input } from '@/components/Input'
+import { Button } from '@/components/Button'
 export const Route = createFileRoute('/')({
   component: App,
 })
@@ -43,16 +44,16 @@ function App() {
     <div className="text-center h-lvh bg-black px-4 py-6 font-all ">
       <div className='flex flex-col h-full items-center gap-16 justify-center text-white'>
         <h1 className='text-5xl font-extrabold w-2/3'>Adicione o seu nome para ter acesso a Agenda virtual!</h1>
-        <input
+        <Input
          onChange={handleNameChange}
          placeholder='Nome' 
          className='p-4 text-gray-900 w-1/3 bg-white border-4 border-blue-700 rounded-full'  type="text" />
         {erro && <p className='text-red-500'>{erro}</p>}
-        <button 
+        <Button 
         onClick={() => handleSave()}
-        className='cursor-pointer bg-green-500 hover:bg-green-400 text-2xl w-1/6 rounded-full p-4'>
+        className=' bg-green-500 text-2xl w-1/6 rounded-full '>
           Salvar
-        </button>
+        </Button>
         </div>
     </div>
   )
