228225296a2db85fdcd508099d5a929c229cc896
VerttB <alyssonoliveira456@gmail.com>
2025-09-07 21:18:55 -0300

    style: mudancas na cor primary

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
 export const Button = ({variant = "primary", className, ...rest}: ButtonProps) => {
 
     const variants = {
-        primary: "bg-gray-500",
+        primary: "bg-gray-400",
         danger: "bg-red-700",
         secondary: "bg-blue-700"
     };
@@ -14,7 +14,7 @@ export const Button = ({variant = "primary", className, ...rest}: ButtonProps) =
     return(
         <button 
         className={twMerge(
-            `rounded-2xl px-4 py-2 cursor-pointer shadow-md shadow-gray-500/50 transition-transform
+            `rounded-full px-8 py-2 cursor-pointer shadow-md shadow-gray-500/50 transition-transform
              duration-200 hover:scale-95 active:scale-[98%]`,variants[variant], className)}
         {...rest}
         />
