39ad229866cb6e986e8c84d6364249a8a7452430
Verttb <alyssonoliveirai456@gmail.com>
2025-09-23 19:30:41 -0300

    style: qualidade visual e logica Quando o componente é fechado campos são resetados Validação de campos Formatação de telefone ao digitar

--- src/components/Contacts/ContactAddItem.tsx
+++ src/components/Contacts/ContactAddItem.tsx
@@ -8,6 +8,7 @@ import { Button } from "../Button";
 import useSWRMutation from "swr/mutation";
 import { useAlert } from "@/core/hooks/useAlert";
 import { Alert } from "../Alert";
+import { formatTel } from "@/core/utils/formatTel";
 
 interface ContactAddItemProps {
   agendaId: string;
@@ -31,14 +32,25 @@ const sendContact = async (url: string, { arg }: { arg: { nome: string; telefone
 }
 
 export const ContactAddItem = ({ onSuccess,agendaId }: { onSuccess?: () => void, agendaId:string }) => {
+
     const [open, setOpen] = useState<boolean>(false)
     const [nome, setNome] = useState("");
     const [telefone, setTelefone] = useState("");    
     const { show, message, type, showAlert, hideAlert } = useAlert();
+    const [error,setError] = useState("");
     
     const { trigger, isMutating } = useSWRMutation("http://localhost:8080/contatos", sendContact);
-
+    console.log("Render Add")
     const handleAdd = async () => {
+      if(nome.trim().length < 2){
+        setError("Nome inválido, no mínimo 2 caracteres")
+        return;
+      }
+      setTelefone(telefone.replace(/\D/g, ''));
+      if(telefone.trim().length != 11){
+        setError("Telefone inválido")
+        return;
+      }
       try{
         await trigger({agendaId, nome, telefone });
         onSuccess?.();
@@ -52,6 +64,12 @@ export const ContactAddItem = ({ onSuccess,agendaId }: { onSuccess?: () => void,
       }
   };
 
+  const closeModal = () => {
+    setNome("");
+    setTelefone("");
+    setError("");
+    setOpen(false);
+  }
     return(
         <>
         <Alert show={show} message={message} type={type} onClose={hideAlert}/>
@@ -61,21 +79,37 @@ export const ContactAddItem = ({ onSuccess,agendaId }: { onSuccess?: () => void,
                   items-center h-16 p-0 w-16 text-2xl
                 hover:bg-gray-100"><span><BsPlus color="black"size={32}/></span></Button>
         <Modal.Root open={open} onOpenChange={setOpen} className="bg-white border-blue-600 border-3">
-            <Modal.Header  onClose={() => setOpen(false)}></Modal.Header>
+            <Modal.Header  onClose={closeModal}></Modal.Header>
             
             <Modal.Content className="min-w-108 min-h-32 items-center gap-8 px-10">
                 <h1 className="text-3xl">Adicione um Contato!</h1>
                 <div className="flex w-full gap-8 items-center">
                     <FaUser size={40} color="blue"/>
-                <Input placeholder="Nome" onChange={(e) => setNome(e.target.value)} className="bg-yellow-50 font-medium text-2xl text-gray-700 border-2 w-full py-0 px-6"></Input>
+                <Input 
+                    placeholder="Nome"
+                    onChange={(e) => setNome(e.target.value)}
+                    className="bg-yellow-50 font-medium text-2xl text-gray-700 border-2 
+                              w-full py-0 px-6"/>
                 </div>
                   <div className="flex w-full items-center gap-8">
-                    <BsTelephoneFill size={40} color="blue"/>
-                <Input placeholder="Telefone" onChange={(e) => setTelefone(e.target.value)} className=" bg-yellow-50 font-medium text-2xl text-gray-700 border-2 w-full py-0 px-6"></Input>
+                <BsTelephoneFill size={40} color="blue"/>
+                <Input 
+                    
+                    placeholder="Telefone"
+                    value={formatTel(telefone)}
+                    onChange={(e) => {
+                      const digits = e.target.value.replace(/\D/g, "").slice(0, 11);
+                      setTelefone(digits);
+                  }}
+                    className="bg-yellow-50 font-medium text-2xl text-gray-700 border-2
+                               w-full py-0 px-6"/>
                 </div>
             </Modal.Content>
-            <Modal.Actions className="text-white w-full flex justify-around p-4 mb-6">
-                <Button className="px-10 py-2">Cancelar</Button>
+            <Modal.Actions className="text-white w-full flex flex-col items-center justify-around p-4 mb-6">
+              <div className="flex gap-4 mb-2">
+                <Button 
+                      className="px-10 py-2"
+                      onClick={closeModal}>Cancelar</Button>
                 <Button
                     className="px-10 py-2"
                     variant="secondary"
@@ -83,7 +117,10 @@ export const ContactAddItem = ({ onSuccess,agendaId }: { onSuccess?: () => void,
                     disabled={isMutating}
                 >
                     {isMutating ? "Adicionando..." : "Confirmar"}
-          </Button>            </Modal.Actions>
+          </Button>
+          </div>
+            {error && <p className="text-red-500">{error}</p>}
+          </Modal.Actions>
         </Modal.Root>
         </>
     )
