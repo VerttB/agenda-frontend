1607242092eee8294bd730c2cddd4b0c4fad220e
Verttb <alyssonoliveirai456@gmail.com>
2025-09-23 19:32:08 -0300

    refactor: alterada mensagem do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <footer className="p-2 bg-secondary flex gap-2  text-black justify-between">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
-          Copyright C
-        </div>
+      <nav className="flex flex-row justify-center w-full text-gray-900">
+        <div className="px-2 font-bold ">
+    © 2025 Empresa Inc          
+    </div>
       </nav>
     </footer>
   )
