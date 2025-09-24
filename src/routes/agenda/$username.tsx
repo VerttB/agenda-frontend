8512893c1138db60337c7bf19d86ad02abb74ff8
VerttB <alyssonoliveira456@gmail.com>
2025-09-24 08:46:34 -0300

    push

--- src/routes/agenda/$username.tsx
+++ src/routes/agenda/$username.tsx
@@ -7,6 +7,29 @@ import useSWR, { mutate } from 'swr'
 import type { Contact } from '@/core/interfaces/Contact'
 import {  useEffect, useState } from 'react'
 import { CgSpinner } from 'react-icons/cg'
+import { BsFillTrash3Fill } from "react-icons/bs"
+import useSWRMutation from 'swr/mutation'
+
+
+
+const deleteManyContacts = async (url: string, { arg }: { arg: {idAgenda:string ,nome: string} }) => {
+
+  const response = await fetch(`${url}/${arg.idAgenda}/contatos/remover`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nome: arg.nome }),
+  });
+  
+  console.log(response.status, typeof response.status);
+  if (response.status < 199 || response.status > 300) {
+    throw new Error("Erro ao excluir contato");
+  }
+
+  if(response.status == 204) return null
+  return response.json();
+};
 
 const labelContacts = (contacts: Contact[]) => {
   const contactMap = new Map<string, Contact[]>();
@@ -49,7 +72,20 @@ function RouteComponent() {
       ? `http://localhost:8080/agenda/${username}/contatos?nome=${encodeURIComponent(debouncedSearch)}&telefone=${encodeURIComponent(debouncedSearch)}`
       : `http://localhost:8080/agenda/${username}/contatos`;
 
+
+  
   const { data, error, isLoading } = useSWR(endpoint, fetcher);
+  const { trigger, isMutating } = useSWRMutation(`http://localhost:8080`, deleteManyContacts);
+
+   const handleDeleteMany = async () => {
+    try{
+      console.log("deletando muitos")
+      await trigger({idAgenda: username, nome: search });
+      mutate(endpoint);
+  }catch (error) {
+      console.error("Erro ao deletar contatos:", error);
+  }
+};
 
   if (error) return <div>Erro ao carregar</div>;
 
@@ -65,6 +101,12 @@ function RouteComponent() {
             value={search}
             onChange={e => setSearch(e.target.value)}
           />
+           <button
+              onClick={handleDeleteMany}
+              title='Deletar contatos pesquisados'>
+            <BsFillTrash3Fill 
+               className='mr-1' size={20} color='red'/>
+               </button>
         </div>
         <div className="xl:w-4/5 w-full flex px-4 items-center overflow-y-auto scrollbar-thin flex-col gap-2 max-h-[60vh]">
         {isLoading && <div className='flex justify-center'><CgSpinner className='animate-spin' size={48} color='white'/></div>}
